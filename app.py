70e55df64e8bbabbeeb4f651a500264a1cf3e5eb
Adeeb Abbas <aa4239@drexel.edu>
2021-04-17 18:21:04 -0400

    init

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-app.run(host='0.0.0.0', port=81)
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
