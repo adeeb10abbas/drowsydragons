e667891cc9d9f60ca375d10e5f4f808e3b8a5bed
Anna Panczner <annapanczner@gmail.com>
2021-04-18 11:40:36 -0400

    added spotify registration

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('dashboard', views.dashboard, name='user-dashboard'),
     path('about/', views.about, name='blog-about'),
+    path('dashboard/spotify', views.register_spotify, name='spotify'),
+    path('dashboard/confirmspotify', views.confirm_spotify, name='confirm-spotify'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import messages
 
 from .models import Drowsy
 
@@ -26,8 +27,57 @@ def driver_is_sleepy(request): #adeeb sends us shit here
 def dashboard(request):
     return render(request, 'blog/dashboard.html')
 
-def register_spotify():
-    pass
+def register_spotify(request):
+    import spotipy
+    from spotipy import oauth2
+    import requests
+
+    CLIENT_ID = "fec22cf923c04b9eae0a62c19bfcd731"
+    CLIENT_SECRET = "bf61a4918571412f920ea14190d8d227"
+
+    credentials = oauth2.SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET, redirect_uri="http://127.0.0.1:8000/dashboard/confirmspotify", scope="user-read-playback-state, user-modify-playback-state")
+
+    token = credentials.get_access_token()
+
+    spotify = spotipy.Spotify(auth=token)
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+token.get('access_token'),
+    }
+    print(headers)
+    return render(request, 'blog/dashboard.html')
+
+def confirm_spotify(request):
+    import requests
+    import base64
+
+    CLIENT_ID = "fec22cf923c04b9eae0a62c19bfcd731"
+    CLIENT_SECRET = "bf61a4918571412f920ea14190d8d227"
+
+    code = str(request.GET['code'])
+    messages.info(request, 'code: '+code)
+
+    #auth_str = 'Basic '+CLIENT_ID+":"+CLIENT_SECRET
+    
+    #headers = {
+    #    'Authorization': auth_str,
+    #}
+
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'http://127.0.0.1:8000/dashboard/confirmspotify'
+    }
+
+    response = requests.post('https://accounts.spotify.com/api/token', auth=(CLIENT_ID, CLIENT_SECRET), data=data)
+    messages.info(request, 'You have successfully registered with Spotify.')
+    access_token = response.json()['access_token']
+
+    return render(request, 'blog/dashboard.html')
 
 def control_spotify():
     pass
