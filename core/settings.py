4b6def65654666d7b05775534b2641c8084744e8
Adeeb Abbas <aa4239@drexel.edu>
2021-04-18 11:58:28 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,8 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 django_heroku.settings(locals())
