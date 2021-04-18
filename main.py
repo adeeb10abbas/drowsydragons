64761b3c9b49924a36a96f310b11af417fd5f152
adeeb_abbas <aa4239@drexel.edu>
2021-04-18 03:10:28 +0100

    add

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from imutils import face_utils
 import imutils
 import dlib
 import cv2
-
+from datetime import datetime
 def eye_aspect_ratio(eye):
 	A = distance.euclidean(eye[1], eye[5])
 	B = distance.euclidean(eye[2], eye[4])
@@ -12,45 +12,53 @@ def eye_aspect_ratio(eye):
 	return ear
 	
 thresh = 0.25
-frame_check = 20
+frame_check = 5
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor(".\shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 cap=cv2.VideoCapture(0)
 flag=0
+data = []
 while True:
 	ret, frame=cap.read()
-	frame = imutils.resize(frame, width=450)
+	# frame = imutils.resize(frame, width=450)
+	frame = cv2.resize(frame, (0,0), fx = 0.5, fy = 0.5)
+
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	subjects = detect(gray, 0)
 	for subject in subjects:
 		shape = predict(gray, subject)
 		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
 		rightEAR = eye_aspect_ratio(rightEye)
 		ear = (leftEAR + rightEAR) / 2.0
+
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
+
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
 		if ear < thresh:
 			flag += 1
-			print (flag)
+			print(flag)
 			if flag >= frame_check:
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				#print ("Drowsy")
+				cv2.putText(frame, "****************ALERT!****************", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "****************ALERT!****************", (10,325), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				print("Drowsy")
+				data.append(["Drowsy",str(datetime.now())])
 		else:
 			flag = 0
+		
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
 		break
+print(data)
 cv2.destroyAllWindows()
 cap.stop()
